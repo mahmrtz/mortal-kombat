89f722c19f646adee8b63885567b842b5010f30a
unknown <proboyisprolol@gmail.com>
2024-12-13 20:18:01 +0400

    final

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function scorpion(){
+    const scorpion = document.getElementById("character");
+    scorpion.src = ("https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/b8a7bbb5-e0ed-4a63-b324-04bee8ca1fbb/dfwm0f1-d600c76c-c373-4d4a-be49-cfbcbd463f38.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2I4YTdiYmI1LWUwZWQtNGE2My1iMzI0LTA0YmVlOGNhMWZiYlwvZGZ3bTBmMS1kNjAwYzc2Yy1jMzczLTRkNGEtYmU0OS1jZmJjYmQ0NjNmMzgucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.nI5y1JUilw_9HYLnrBUbdP7yntAm6Md_013HVwIXhEU")
+}
+
+
+function subzero(){
+    const subzero = document.getElementById("character")
+    subzero.src = ("https://w0.peakpx.com/wallpaper/387/436/HD-wallpaper-scorpion-subzero-mortal-kombact-thumbnail.jpg")
+}
+
+
+function raiden(){
+    const raiden = document.getElementById("character"); 
+    raiden.src = ("https://img1.picmix.com/output/stamp/normal/2/9/5/1/2541592_7f4d2.gif")
+}
+
+function ermac(){
+    const ermac = document.getElementById("character");
+    ermac.src = ("https://64.media.tumblr.com/dc29d950464bbeae9ad0b6680e2a6e43/ba638fc3074d3d78-76/s250x400/f2c37b68a2d2dd19587174899d724a388051f2e2.gif")
+}
